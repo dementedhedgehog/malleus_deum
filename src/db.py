cb2e7e3beb53d33328cd8fd03d5807423875d088
blaize <blaize@pobox.com>
2017-12-19 10:06:49 +1000

    Added NPCs for encounters.

---
+++ src/db.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+"""
+
+  This represents the game state.
+
+
+"""
+from os.path import abspath, join, splitext, dirname, exists, basename
+
+from abilities import AbilityGroups
+from monsters import MonsterGroups
+from archetypes import Archetypes
+from patrons import Patrons
+from npcs import NPCGangs
+
+class DB:
+
+    def __init__(self):
+        self.patrons = None
+        self.ability_groups = None
+        self.monster_groups = None
+        self.archetypes = None
+        return
+
+    
+    def load(self, root_dir, fail_fast=True):
+        # load the abilities
+        abilities_dir = join(root_dir, "abilities")
+        self.ability_groups = AbilityGroups()
+        self.ability_groups.load(abilities_dir, fail_fast=fail_fast)
+        
+        # load the archetypes
+        archetype_dir = join(root_dir, "archetypes")
+        self.archetypes = Archetypes()
+        self.archetypes.load(self.ability_groups, archetype_dir, fail_fast=fail_fast)
+
+        # load the monsters
+        monsters_dir = join(root_dir, "monsters")
+        self.monster_groups = MonsterGroups()
+        self.monster_groups.load(monsters_dir, fail_fast=fail_fast)
+
+        # load the patrons
+        patrons_dir = join(root_dir, "patrons")
+        self.patrons = Patrons()
+        self.patrons.load(patrons_dir=patrons_dir,
+                          ability_groups=self.ability_groups,
+                          fail_fast=fail_fast)
+
+        # load the npcs
+        npcs_dir = join(root_dir, "encounters") # FIXME
+        self.npc_gangs = NPCGangs()        
+        self.npc_gangs.load(npcs_dir=npcs_dir,
+                            monster_groups=self.monster_groups,
+                            fail_fast=fail_fast)
+        return
+
---
+++ src/npcs.py
@@ -0,0 +1,503 @@
+#!/usr/bin/env python
+from os.path import join
+from os import listdir, walk
+import os
+
+from utils import (
+    parse_xml, validate_xml, children_to_string, contents_to_string,
+    COMMENT, convert_str_to_int, convert_to_roman_numerals,
+    root_dir
+)
+
+from monsters import MonsterGroups
+
+
+class NPC:
+    """
+    NPC info shared between NPC and SimpleNPC.
+
+    """
+
+    def __init__(self, monster_groups, npc_group=None):
+        self.monster_groups = monster_groups
+
+        # These values shadow those in the monster class
+        # we use these to enable people to override the
+        # default monster values
+        self.ac = None
+        self.name = None
+        self.move = None
+        self.magic_pool = None
+        self.initiative_bonus = None
+        self.resolve = None
+        self.monster_id = None
+        self.monster = None
+        self.strength = None
+        self.endurance = None
+        self.agility = None
+        self.speed = None
+        self.luck = None
+        self.willpower = None
+        self.perception = None
+        self.name = None
+        self.health = None
+        self.stamina = None
+
+        self.ability_level_ids = []
+        self.npc_group = npc_group        
+        return
+    
+    def is_npc_group(self):
+        return False
+
+    def get_name(self):
+        return self.name
+    
+    def get_monster_id(self):
+        return self.monster_id
+
+    def get_initiative_bonus(self):
+        return self.monster.get_initiative_bonus()
+
+    def get_tags_str(self):
+        return self.monster.get_tags_str()
+    
+    def get_title(self):
+        return self.monster.get_title()    
+
+    def get_stamina(self):
+        return self.monster.get_stamina()
+    
+    def get_health(self):
+        return self.monster.get_health()
+
+    def get_aspects_str(self):
+        return self.monster.get_aspects_str()    
+
+    def get_description(self):
+        return self.monster.get_description()
+    
+    def get_ability_level_ids(self):
+        result = self.ability_level_ids            
+        if self.monster is not None:
+            result += self.monster.get_ability_level_ids()
+        return result
+    
+    def get_magic_pool(self):
+        result = None
+        if self.magic_pool is not None:
+            result = self.magic_pool
+        elif self.monster is not None:
+            result = self.monster.magic_pool
+        return result
+    
+    def get_resolve_pool(self):
+        result = None
+        if self.resolve is not None:
+            result = self.resolve
+        elif self.monster is not None:
+            result = self.monster.resolve
+        return result
+    
+    def get_ac(self):
+        result = None
+        if self.ac is not None:
+            result = self.ac
+        elif self.monster is not None:
+            result = self.monster.ac
+        return result
+
+    def get_move(self):
+        result = None
+        if self.move is not None:
+            result = self.move
+        elif self.monster is not None:
+            result = self.monster.move
+        return result
+
+    def get_strength(self):
+        result = None
+        if self.strength is not None:
+            result = self.strength
+        elif self.monster is not None:
+            result = self.monster.strength
+        return result
+
+    def get_endurance(self):
+        result = None
+        if self.endurance is not None:
+            result = self.endurance
+        elif self.monster is not None:
+            result = self.monster.endurance
+        return result
+
+    def get_agility(self):
+        result = None
+        if self.agility is not None:
+            result = self.agility
+        elif self.monster is not None:
+            result = self.monster.agility
+        return result
+
+    def get_speed(self):
+        result = None
+        if self.speed is not None:
+            result = self.speed
+        elif self.monster is not None:
+            result = self.monster.speed
+        return result
+
+
+    def get_luck(self):
+        result = None
+        if self.luck is not None:
+            result = self.luck
+        elif self.monster is not None:
+            result = self.monster.luck
+        return result
+
+
+    def get_willpower(self):
+        result = None
+        if self.willpower is not None:
+            result = self.willpower
+        elif self.monster is not None:
+            result = self.monster.willpower
+        return result
+
+
+    def get_perception(self):
+        result = None
+        if self.perception is not None:
+            result = self.perception
+        elif self.monster is not None:
+            result = self.monster.perception
+        return result
+
+    
+    def parse(self, node):        
+         for child in list(node):
+            tag = child.tag
+
+            if tag == "monsterid":
+                self.monster_id = child.text
+                self.monster = self.monster_groups.get_monster_by_id(self.monster_id)
+
+            elif tag == "name":
+                self.name = child.text
+
+            elif tag == "resolve":
+                self.health = child.text
+
+            elif tag == "magic_pool":
+                self.health = child.text
+                
+            elif tag == "health":
+                self.health = child.text
+
+            elif tag == "stamina":
+                self.stamina = child.text
+
+            else:
+                raise Exception("UNKNOWN (%s) %s\n" % (child.tag, str(child)))
+         return
+
+
+class NPCGroup:
+    """
+    A group of npcs of the *same* monster type..
+
+    """
+    def __init__(self, monster_groups):
+        self.npcs = []
+        self.monster_id = None
+        self.monster_groups = monster_groups
+        return
+
+    def get_monster_id(self):
+        return self.monster_id
+
+    def get_title(self):
+        return self.monster.get_title()
+
+    def get_tags_str(self):
+        return self.monster.get_tags_str()
+
+    def get_initiative_bonus(self):
+        return self.monster.get_initiative_bonus()    
+        
+    def get_strength(self):
+        return self.monster.strength
+
+    def get_endurance(self):
+        return self.monster.endurance
+
+    def get_agility(self):
+        return self.monster.agility
+
+    def get_speed(self):
+        return self.monster.speed
+
+    def get_luck(self):
+        return self.monster.luck
+
+    def get_willpower(self):
+        return self.monster.willpower
+
+    def get_perception(self):        
+        return self.monster.perception
+
+    def get_ability_level_ids(self):
+        return self.monster.get_ability_level_ids()
+
+    def get_ac(self):
+        return self.monster.ac
+
+    def get_stamina(self):
+        return self.monster.get_stamina()
+
+    def get_health(self):
+        return self.monster.get_health()
+    
+    def get_resolve_pool(self):
+        return self.monster.resolve
+
+    def get_magic_pool(self):
+        return self.monster.magic_pool
+
+    def get_aspects_str(self):
+        return self.monster.get_aspects_str()    
+    
+    def get_move(self):
+        return self.monster.move
+    
+    def __iter__(self):
+        return iter(self.npcs)
+
+
+    def parse(self, npcgroup):
+        for child in list(npcgroup):
+           tag = child.tag
+
+           if tag == "monsterid":
+               self.monster_id = child.text
+               self.monster = self.monster_groups.get_monster_by_id(self.monster_id)
+               self.monster = self.monster_groups.get_monster_by_id(self.monster_id)
+
+           elif tag == "npc":
+               npc = NPC(monster_groups=self.monster_groups, npc_group=self)
+               npc.parse(child)
+               self.npcs.append(npc)
+
+           elif tag is COMMENT:
+               # ignore comments!
+               pass
+           else:
+               raise Exception("UNKNOWN (%s) %s\n" % (child.tag, str(child)))
+
+        for npc in self.npcs:
+            npc.monster = self.monster
+        return
+
+    def is_npc_group(self):
+        return True
+
+
+    
+class NPCGang:
+    """
+    A list of npc_likes (i.e. a list of npc or npc_groups).
+
+    """
+
+    # map of npc_id -> npcs.
+    npc_like_lookup = {}
+
+    def __init__(self, monster_groups):
+        self.npc_likes = []
+        self.npcs_id = None
+        self.monster_groups = monster_groups
+        return
+
+    def __iter__(self):
+        return iter(self.npc_likes)
+
+    def __getitem__(self, key):
+        return self.npc_likes[key]   
+
+    def get_id(self):
+        return self.npcs_id
+
+    def parse(self, npcs_node):
+        for child in list(npcs_node):
+           tag = child.tag
+
+           if tag == "npcsid":
+               self.npcs_id = child.text
+
+           elif tag == "npc":
+               npc = NPC(monster_groups=self.monster_groups)
+               npc.parse(child)
+               self.npc_likes.append(npc)
+
+           elif tag == "npcgroup":
+               npc_group = NPCGroup(monster_groups=self.monster_groups)
+               npc_group.parse(child)
+               self.npc_likes.append(npc_group)
+
+           elif tag is COMMENT:
+               # ignore comments!
+               pass
+           else:
+               raise Exception("UNKNOWN (%s) %s\n" % (child.tag, str(child)))
+        return
+
+    @classmethod
+    def load(cls, npcs_dir, monster_groups, fail_fast):
+        result = True
+
+        for root, dirs, files in walk(npcs_dir, topdown=False):
+            for name in files:            
+                if not name.endswith(".xml"):
+                    continue
+
+                if name.startswith(".#"):
+                    continue
+            
+                xml_fname = join(root, name)
+                print xml_fname
+                
+                doc = parse_xml(xml_fname)
+                root = doc.getroot()
+
+                print root.tag
+
+                if root.tag == "npcs":
+                    print "XXX"
+
+                    npcs = NPCs(monster_groups=monster_groups)
+                    npcs.parse(root)
+                    if not npcs.validate():
+                        result = False
+                        if fail_fast:
+                            raise Exception("Errors in %s" % xml_fname)                
+                    else:
+                        cls.npc_like_lookup[npcs.npcs_id] = npcs
+        return result
+    
+
+    def validate(self):
+        # FIXME: placeholder
+        return True
+
+    def is_npc_group(self):
+        return True
+
+
+class NPCGangs:
+    """
+    A lookup table of npc_gangs.
+
+    """
+
+    def __init__(self): # , monster_groups):
+        #self.npc_likes = []
+        #self.npcs_id = None
+        #self.monster_groups = monster_groups
+        
+        # map of npc_id -> npcs.
+        self.npc_gang_lookup = {}        
+        return
+
+    def __iter__(self):
+        return iter(self.npc_gang_lookup.items())
+
+
+    # def __index__(self, key):
+    #     return self.npc_gang_lookup[key]
+
+    def __getitem__(self, key):
+        print "xxxxxxxxxxxxxxxxxxx =============================="
+        print key
+        print self.npc_gang_lookup[key]   
+        return self.npc_gang_lookup[key]   
+    
+    
+    # def get_id(self):
+    #     return self.npcs_id
+
+    # def parse(self, npcs_node):
+    #     for child in list(npcs_node):
+    #        tag = child.tag
+
+    #        if tag == "npcsid":
+    #            self.npcs_id = child.text
+
+    #        elif tag == "npc":
+    #            npc = NPC(monster_groups=self.monster_groups)
+    #            npc.parse(child)
+    #            self.npc_likes.append(npc)
+
+    #        elif tag == "npcgroup":
+    #            npc_group = NPCGroup()
+    #            npc_group.parse(child)
+    #            self.npc_likes.append(npc_group)
+
+    #        elif tag is COMMENT:
+    #            # ignore comments!
+    #            pass
+    #        else:
+    #            raise Exception("UNKNOWN (%s) %s\n" % (child.tag, str(child)))
+    #     return
+
+    def load(self, npcs_dir, monster_groups, fail_fast):
+        result = True
+        
+        # load all the monster groups
+        for root, dirs, files in os.walk(npcs_dir, topdown=False):
+
+            for name in files:
+                if not name.endswith(".xml"):
+                    continue
+
+                if name.startswith(".#"):
+                    continue
+
+                xml_fname = os.path.join(root, name)
+
+                doc = parse_xml(xml_fname)
+                doc_root = doc.getroot()
+                if doc_root.tag == "npcs":
+                    npcs = NPCGang(monster_groups=monster_groups)
+                    npcs.parse(doc_root)
+                    if not npcs.validate():
+                        result = False
+                        if fail_fast:
+                            raise Exception("Errors in %s" % xml_fname)                
+                    else:
+                        self.npc_gang_lookup[npcs.npcs_id] = npcs
+        return result
+    
+
+    def validate(self):
+        # FIXME: placeholder
+        return True
+
+
+if __name__ == "__main__":
+    fail_fast = True
+
+    monsters_dir = join(root_dir, "monsters")
+    monster_groups = MonsterGroups()
+    monster_groups.load(monsters_dir, fail_fast=fail_fast)
+    
+    npcs_dir = join(root_dir, "encounters")
+    npc_gangs = NPCGangs()
+    npc_gangs.load(npcs_dir=npcs_dir,
+                   monster_groups=monster_groups,
+                   fail_fast=fail_fast)
+    
+    for npc_like in npc_gangs:
+        print npc_like
+
+    print 
