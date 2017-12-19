b4ea519b2f3b61fa0772025af2e5a94d1db9042b
blaize <blaize@pobox.com>
2017-12-19 11:06:26 +1000

    Writes a report on the images .. to make it easy to make sure that they're open source.

--- src/check_licenses.py
+++ src/check_licenses.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # coding=utf-8
 """
 
@@ -9,70 +9,105 @@
 """
 import os
 from os.path import abspath, join, splitext, dirname, exists, basename
-from PIL import Image, ExifTags
+import json
+
+fail_fast = False
 
 
+def fail(msg):
+    if fail_fast:
+        raise Exception(msg)
+    else:
+        print msg
+    return
+        
 
 
-class Report:
+class ImgInfo:
+    info_lookup = {}
 
-    def __init__(self, fname, resource_license, artist, source):
-        self.fname = fname
-        self.license = resource_license
-        self.artist = artist
-        self.source = source
+    def __init__(self):
+        self.img_fnames = set()
+        self.img_license = None # resource_license
+        self.artist = None # artist
+        self.source = None # source
         return
 
+    @classmethod
+    def parse(cls, img_fname, license_fname):        
+        key = basename(license_fname)
+
+        if key in cls.info_lookup:
+            info = cls.info_lookup[key]
+        else:
+            info = ImgInfo()
+            cls.info_lookup[key] = info
+            
+        info.img_fnames.add(img_fname)
+
+
+        if not exists(license_fname):
+            fail("License file missing %s" % license_fname)
+            return
+        
+        f = file(license_fname)
+        json_info = json.load(f)
+        for attr, value in json_info.items():
+            if attr == "license":
+                info.img_license = value
+            elif attr == "artist":
+                info.artise = value
+            elif attr == "source":
+                info.source = value
+            else:
+                fail("Unknown license information %s in %s" %
+                     (attr, license_fname))
+
+        if info.img_license is None:
+            fail("Missing license information 'license' in %s" %
+                 license_fname)
+
+        if info.artist is None:
+            fail("Missing license information 'artist' in %s" %
+                 license_fname)
+
+        if info.source is None:
+            fail("Missing license information 'source' in %s" %
+                 license_fname)
+        return
+
+    def __str__(self):
+        return "%s %s %s %s" % (
+            ", ".join(self.img_fnames),
+            self.img_license,
+            self.artist,
+            self.source)
+    
 
 def generate_license_report(project_dir):
 
     for dir_name, sub_dirs, files in os.walk(project_dir):
-        print dir_name
+        #print dir_name
         for fname in files:
-
-            _, ext = splitext(fname)
-            full_fname = join(dir_name, fname)
             
-            if ext in ("", ".ods", ".bat", ".md", ):
-                # ignore these non-image files!
-                pass
+            base_fname, ext = splitext(fname)
+            full_fname = join(dir_name, fname)
+
+            # images..
+            if ext in (".png", ".svg", ".jpg", "gif", "xcf"):
+                license_fname = join(dir_name, base_fname + ".json")
+                ImgInfo.parse(img_fname=full_fname,
+                              license_fname=license_fname)
                 
-            elif ext in (): # (".png", ".svg", ".jpg"):
-                print('\t%s' % fname)
-
-            elif ext == ".jpg":
-                #):
-                print('\t%s' % fname)
-
-                img = Image.open(full_fname)
-                #exif = { ExifTags.TAGS[k]: v
-                #         for k, v in img._getexif().items()
-                #         if k in ExifTags.TAGS }
-                #print exif
-
-                info = img._getexif()
-                print info
-
-
-            elif ext == ".svg":
-                print('\t%s' % fname)
-                
-                
-            elif ext in (".ttf", ):
-                print('\t%s' % fname)
-
-            else:
-                raise Exception("Unhandled extension %s for file %s"
-                                % (ext, fname))
-                
-        if '.git' in sub_dirs:
-            sub_dirs.remove('.git')
-
-
+    return
 
 
 if __name__ == "__main__":
     src_dir = dirname(__file__)
     project_dir = abspath(join(src_dir, ".."))
     generate_license_report(project_dir)
+
+    print "----"
+    for img_info in ImgInfo.info_lookup.values():
+        print img_info
     
