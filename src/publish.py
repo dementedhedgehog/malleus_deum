33203b6d72472d7cbeec071f5732661c80e40313
dementedhedgehog <dementedhedgehog@gmail.com>
2023-05-18 09:04:54 +1000

    Fix build .. we weren't always building all the graphs we needed

--- src/publish.py
+++ src/publish.py
@@ -41,10 +41,11 @@ from latex_formatter import LatexFormatter
 from html_formatter import HtmlFormatter
 from spreadsheet_writer import write_game_balance_spreadsheet
 
-# Creates graphs as a side effect of the imports (FIXME: yucky.. fix it later)
+# Graph creation stuff.. for analysis in the rationale doc.
 import graphs
 import dice_pool_graph
 import morale_graph
+import d6_graph
 
 # FIXME: didn't want to deal with pdftk at the moment.
 from character_sheet_writer import (
@@ -666,13 +667,13 @@ if __name__ == "__main__":
     if not exists(pdfs_dir):
         mkdir(pdfs_dir)
 
-    # Conditionally build some graphs we mostly won't need?
+    # Conditionally build some graphs (we won't need unless we're building the rationale doc)
     if "rationale.xml" in [t[0] for t in config.doc_files_to_build]:
         print("Building dice pool graphs.")
+        graphs.draw_graphs()
         dice_pool_graph.build_dice_pool_graphs()
-
-        # build the morale graph if it is missing.
         morale_graph.build_morale_graph()
+        d6_graph.draw_d6_graph()
 
     # load the game database (archetypes, abilties etc).
     db = DB()
